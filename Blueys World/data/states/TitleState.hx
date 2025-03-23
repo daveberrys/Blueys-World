c368ca91ba9e09e72176465885ab94c12e1c7d9a
BaranMuzu <baranfnaf12@gmail.com>
2025-03-24 00:31:51 +0300

    title menu. [gift ig]
    hey dave instead of eepying i wanted to make this for you i hope you would like it :3 while youre reading this im prob gonna be in school so. take care
    
    - added new title state

---
+++ Blueys World/data/states/TitleState.hx
@@ -0,0 +1,58 @@
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
+
+var grid:FlxBackdrop;
+var titlebg:FlxSprite;
+var titlebluey:FlxSprite;
+var titleLogo:FlxSprite;
+var titlelogotween:FlxTween;
+var titleText:FlxSprite;
+var titleTextTween:FlxTween;
+var titleblueytween:FlxTween;
+
+function postCreate() {
+    titleLogo = new FlxSprite(-40,0).loadGraphic(Paths.image('menus/titlescreen/titlelogo'));
+	insert(3, titleLogo);
+
+    titlebluey = new FlxSprite(500,0).loadGraphic(Paths.image('menus/titlescreen/titlebluey'));
+	insert(2, titlebluey);
+
+    titlebg = new FlxSprite(0,0).loadGraphic(Paths.image('menus/titlescreen/titlebg'));
+	insert(1, titlebg);
+
+    titleText = new FlxSprite(100,300).loadGraphic(Paths.image('menus/titlescreen/titletext'));
+	insert(6, titleText);
+    
+    grid = new FlxBackdrop(FlxGridOverlay.createGrid(40, 40, 80, 80, true, 0x77FFFFFF, 0x335419FFF));
+    grid.velocity.set(40, 40);
+    insert(2, grid);
+}
+
+function beatHit(curBeat) {
+    if (skippedIntro == true) {
+        FlxTween.tween(titlebluey, {x:0}, 2, {ease: FlxEase.expoOut});
+    }
+    
+    titlebluey.y = 30;
+
+    if (titleblueytween != null) titleblueytween.cancel();
+    titleblueytween = FlxTween.tween(titlebluey, {y: 0}, 1, {ease: FlxEase.expoOut});
+
+    titleText.scale.x = 1;
+    titleText.scale.y = 1;
+
+    if (titleTextTween != null) titleTextTween.cancel();
+    titleTextTween = FlxTween.tween(titleText.scale, {x: 0.9, y: 0.9}, 1, {ease: FlxEase.expoOut});
+
+    if (curBeat % 2 == 0) {
+        titleLogo.scale.x = 1;
+        titleLogo.scale.y = 1;
+
+        if (titlelogotween != null) titlelogotween.cancel();
+        titlelogotween = FlxTween.tween(titleLogo.scale, {x: 0.9, y: 0.9}, 1, {ease: FlxEase.expoOut});
+    }
+    
+    switch (curBeat) {
+        case 0:
+    }
+}
