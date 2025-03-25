601fbd40bff3549bc552347269308c9556834dbb
Daveberry <84567826+daveberrys@users.noreply.github.com>
2025-03-25 21:10:02 +0800

    ILL FIX IT LATER I SWEARRR (Change Icon.hx and Change Icon.json Edit)
    ILL FIX IT LATER I SWEARRR (Change Icon.hx and Change Icon.json Edit)

--- Blueys World/data/events/Change Icon.hx
+++ Blueys World/data/events/Change Icon.hx
@@ -6,19 +6,9 @@ function onEvent(e) {
         trace(params[0]);
         trace(params[1]);
         trace(params[2]);
+        trace(params[3]);
 
-        var leftColor = dad.color;
-        var rightColor = boyfriend.color;
-
-        if (params[0] == "boyfriend") {
-            varFakeIconPlayer = params[1];
-            PlayState.rightColor = params[2];
-            PlayState.healthBar.createFilledBar(leftColor, rightColor);
-        }
-        else if (params[0] == "dad") {
-            varFakeIconOpponent = params[1];
-            PlayState.leftColor = params[2];
-            PlayState.healthBar.createFilledBar(leftColor, rightColor);
-        }
+        varFakeIconPlayer = params[0];
+        varFakeIconOpponent = params[2];
     }
 }
