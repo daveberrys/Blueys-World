e4f562879e4eff1655d4d34985eff1b4b9eb89df
Daveberry <84567826+daveberrys@users.noreply.github.com>
2025-04-19 01:28:42 +0800

    WHY IS LINUX SO FUCKING CASE SENSITIVE (also made double duo visual a little bit kind of better)

--- data/events/Change Dad Icon.hx
+++ data/events/Change Dad Icon.hx
@@ -2,6 +2,13 @@ function onEvent(e) {
     if (e.event.name == "Change Dad Icon") {
         var params:Array = e.event.params;
 
+        #if !linux
         varFakeIconOpponent = params[0];
+        #else
+        if (params[0] == "Bingo")
+            varFakeIconOpponent = "bingo";
+        else if (params[0] == "Bluey")
+            varFakeIconOpponent = "bluey";
+        #end
     }
 }
